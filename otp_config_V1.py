a7e1f1217810dae314bbfac154e2b587fc570eea
sahil3033 <sahilpawar7719881716@gmail.com>
2023-10-19 12:45:08 +0530

    version 3

--- otp_config_V1.py
+++ otp_config_V1.py
@@ -1,34 +1,100 @@
-from twilio.rest import Client
-import otp_config_V1 as codes
+#Importing requires libraries
 import random
-# OTP to mail
-
+import re
 import smtplib
-import random
+from twilio.rest import Client
 
-sender = "sahilpawar7719881716@gmail.com"
-password = "vzkq xigo ujug csxl"
-receiver = "mercy30032003@gmail.com"
-body = "Your OTP is " + str(random.randint(100000, 999999)) + ". Valid for next 15 minutes."
+#User and Twilio info
+account_sid = 'AC99a50feedf725cb4b7cce68cc12c2630'
+auth_token = '6c760dc97fa7d15148f5fbfb8b9be96f'
+client = Client(account_sid, auth_token)
+twilio_num = '+17066214970'
+target = '+918983473033'
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+#Function to generate OTP
+def generateOtp(n = 6):
+    otp = ""
 
-server.starttls()
+    for i in range(n):
+        otp += str(random.randint(0, 9))
+    
+    return otp
 
-server.login(sender, password)
+#Function to validate mobile
+def validateMobile(mobile):
+    return len(mobile) == 10 and mobile.isdigit()
 
-server.sendmail(sender, receiver, body)
+#Function to validate Email
+def validateEmailID(receiver):
+    # not efficient way
+    # if "@" not in receiver or "." not in receiver:
+    #     return False
+    # return True
 
-print("Mail sent")
-client = Client(codes.account_sid, codes.auth_token)
+    #efficient way - good practice
+    validation_condition = r"^[\w\.-]+@[\w\.-]+\.\w+$"  # validation using regular expression
+    if re.search(validation_condition, receiver):
+        return True
+    else:
+        return False
 
-message = client.messages.create(
+# Send OTP over mobile using Twilio
+def sendOTPOverMobile(target, otp):
+    if(validateMobile(target)):
+        target = "+91" + target
+        message = client.messages.create(
+            body = "Your OTP is " + otp + ". Valid for next 15 minutes.",
+            from_= twilio_num,
+            to=target
+        )
 
-    body = "Your OTP is " + str(random.randint(100000, 999999)) + ". Valid for next 15 minutes.",
-    from_= codes.twilio_num,
-    to=codes.target
+        print(message.body)
+        print("Check Phone! Sent to ", target)
+    else:
+        print("Enter valid mobile number!!")
 
-)
+# Send OTP over email using SMTP
+def sendOTPOverEmail(receiver, otp):
+    body = "Your OTP is " + otp + ". Valid for next 15 minutes."
 
-print(message.body)
-print("Check Phone for OTP!")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, receiver, body)
+
+    print("Mail sent - OTP: ", otp)
+
+
+#Driver Code
+if __name__ == "__main__":
+
+    print("Welcome to Random OTP sender!!\nHere, we send random OTPs to phone number and mails.\n")
+
+    sender = "sahilpawar7719881716@gmail.com"
+    password = "vzkq xigo ujug csxl"
+    # User input for email
+    receiver = input("Enter mail: ")
+
+    #create a unique Otp
+    otp = generateOtp(6)
+
+    #Send OTP over Email
+    if(validateEmailID(receiver)):
+        sendOTPOverEmail(receiver, otp)
+    else:
+        print("Please Enter valid mail!!")
+
+
+    #Send OTP over SMS
+    send_twilio = input("\nDo you want to send OTP via SMS: ")
+    if send_twilio.lower() == "yes":
+        # User input for mobile
+        target = input("Enter mobile: ")
+
+        sendOTPOverMobile(target, otp)
+        
+        print("\nOTP sending program ended\n")
+    else:
+        print("OTP sending program ended")
+
+    #Program Ended
